2ed94cfb4a8d4c7bc39e9fc869dcae8b3619e906
Codewithmohit56 <mohit.maurya2905@gmail.com>
2026-03-21 21:07:12 +0530

    js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Auto-expand textarea as user types
+const textarea = document.getElementById('message');
+if (textarea) {
+    textarea.addEventListener('input', function() {
+        this.style.height = 'auto';
+        this.style.height = (this.scrollHeight) + 'px';
+    });
+}
